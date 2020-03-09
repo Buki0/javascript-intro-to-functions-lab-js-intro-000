3aa25f680d8e624700767fa1ca33d7573710adb0
interpreted-network-8638 <muratibuki@gmail.com>
2020-03-09 17:32:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,9 +20,15 @@ function sayHiToGrandma(string){
   {
     return "I can't hear you!"
   }
-  else
+  else if(string===string.toUpperCase())
   {
     return "YES INDEED"
   }
+  else if(string.startsWith("I love you, Grandma"))
+  {
+    return "I love you,too"
+  }
+  
+  }
 
-}
+
