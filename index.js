9657305c18896f6a95f15eee00b1efb628297575
interpreted-network-8638 <muratibuki@gmail.com>
2020-03-09 17:24:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,3 +14,10 @@ function logShout(string)  {
 function logWhisper(string){
   return console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string){
+  if(string===string.toLowerCase())
+  {
+    return "I can't hear you!"
+  }
+}
